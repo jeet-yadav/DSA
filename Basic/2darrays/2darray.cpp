4612b66232b7c29672b7e747ff18ed41b29a076c
jeet-yadav <jeetyadav76468976@gmail.com>
2024-07-13 19:15:39 +0530

    added new function

--- Basic/2darrays/2darray.cpp
+++ Basic/2darrays/2darray.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 int print(int arr[][3], int row, int col){// print an 2D-array
+    cout<<"printing array......"<<endl;;
     for(int i=0; i<row; i++){
         for(int j=0; j<col; j++){
             cout<<arr[i][j];
@@ -25,20 +26,34 @@ void search(int arr[][3], int row, int col, int target){//              to searc
         cout<<"Element not found :("<<endl;
     }
 }
+
 void rowWiseSum(int arr[][3], int row, int col){
-    
+    int sum = 0;
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            sum += arr[i][j];
+        }
+        cout<<"Sum of "<<i<<" row : "<<sum<<endl;
+        sum = 0;
+    }
 }
+
 int main(){
     int arr[3][3];
     int arr2[3][3] = {{1,11,111}, {2,22,222}, {3,33,333}};//Hardcode
-    cout << "Enter elements row-wise for a 3x3 array:" << endl;
+    cout<<"Enter elements row-wise for a 3x3 array:"<<endl;
+
     for(int i=0; i<3; i++){
         for(int j=0; j<3; j++){
             cin>>arr[i][j];//row wise input in 2d array
         }
     }
-    int target = 5;
+
+    int target;
+    cout<<"enter element for Searching : ";
+    cin>>target;
     search(arr, 3, 3, target);
     print(arr,3,3);
+    rowWiseSum(arr,3,3);
     return 0;
 }
