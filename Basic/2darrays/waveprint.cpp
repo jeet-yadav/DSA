45c865b0dc1982fd329ba8bc6105506bde82bc72
jeet-yadav <jeetyadav76468976@gmail.com>
2024-07-14 13:57:11 +0530

    DONE

--- Basic/2darrays/waveprint.cpp
+++ Basic/2darrays/waveprint.cpp
@@ -6,40 +6,40 @@ int main(){
 
     for(int i=0; i<3; i++){
         for(int j=0; j<3; j++){
-            cin>>arr[i][j];
+            cin>>arr[i][j];//   taking input in 2d array
         }
     }
  
-    // for(int i=0; i<3; i++){
-    //     if(i%2!=0){
-    //         for(int j=2; j>=0; j--){
-    //             cout<<arr[i][j];
-    //             cout<<" ";
-    //         }
-    //     }
-    //     else{
-    //         for(int j=0; j<3; j++){
-    //             cout<<arr[i][j];
-    //             cout<<" ";
-    //         }
-    //     }
-    //     cout<<endl;
-    // }
-
-    for(int col=0; col<3; col++){
-        if(col%2==0){
-            for(int row=2; row>=0; row--){
-                cout<<arr[row][col];
+    for(int i=0; i<3; i++){//   outer loop for each row
+        if(i%2!=0){
+            for(int j=2; j>=0; j--){// inner loop for odd row
+                cout<<arr[i][j];
                 cout<<" ";
             }
         }
         else{
-            for(int row=0; row<3; row++){
-                cout<<arr[row][col];
+            for(int j=0; j<3; j++){// inner loop for even row
+                cout<<arr[i][j];
                 cout<<" ";
             }
         }
         cout<<endl;
     }
+
+    // for(int col=0; col<3; col++){ //TO print column wise wave
+    //     if(col%2==0){
+    //         for(int row=2; row>=0; row--){
+    //             cout<<arr[row][col];
+    //             cout<<" ";
+    //         }
+    //     }
+    //     else{
+    //         for(int row=0; row<3; row++){
+    //             cout<<arr[row][col];
+    //             cout<<" ";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
     return 0;
 }
