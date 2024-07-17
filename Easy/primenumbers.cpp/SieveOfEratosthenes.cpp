483addd7ff5c51608d9399a712e4e7ea9216ed95
jeet-yadav <jeetyadav76468976@gmail.com>
2024-07-17 16:14:09 +0530

    added Sieve of eartosthenes

---
+++ Easy/primenumbers.cpp/SieveOfEratosthenes.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n, count = 0;
+    cout<<"Enter a number : ";
+    cin>>n;
+    vector<bool> prime(n+1, true); // a bool type vector named prime of size n+1, all the elements are initialised as true for now
+    
+    prime[0] = prime[1] = false; // since 0 and 1 are not prime numbers
+    
+    for(int i=2; i<n; i++){
+        if(prime[i]){
+            count++;
+            
+            for(int j=2*i; j<n; j+=i){
+                prime[j] = 0;
+            }
+        }
+    }
+    cout<<count;
+    return 0;
+}
