871de1ec3bc43fa4402209a4ceec68da0611a48e
jeet-yadav <jeetyadav76468976@gmail.com>
2024-07-17 16:18:06 +0530

    using functions

--- Easy/primenumbers.cpp/SieveOfEratosthenes.cpp
+++ Easy/primenumbers.cpp/SieveOfEratosthenes.cpp
@@ -1,10 +1,9 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int main(){
-    int n, count = 0;
-    cout<<"Enter a number : ";
-    cin>>n;
+
+int SieveOfEratosthenes(int n){
+    int count = 0;
     vector<bool> prime(n+1, true); // a bool type vector named prime of size n+1, all the elements are initialised as true for now
     
     prime[0] = prime[1] = false; // since 0 and 1 are not prime numbers
@@ -19,5 +18,13 @@ int main(){
         }
     }
     cout<<count;
+    return 0;
+}
+
+int main(){
+    int n, count = 0;
+    cout<<"Enter a number : ";
+    cin>>n;
+    SieveOfEratosthenes(n);
     return 0;
 }
