813044ce3caf09cc6e31f5a938c58bb90b276d6e
jeet-yadav <jeetyadav76468976@gmail.com>
2024-07-17 14:14:18 +0530

    max optimization

--- Easy/prime.cpp
+++ Easy/prime.cpp
@@ -2,23 +2,24 @@
 #include <cmath>
 using namespace std;
 
-int prime(int n){
+bool prime(int n){
     if(n<2){
-        return 0;
+        return false;
     }
     for(int i=2; i<=sqrt(n); i++){
         if(n%i==0){
-            return 0;
+            return false;
         }
     }
-    return 1;
+    return true;
 }
 
 int main(){
     int n, count = 0;
+    cout<<"Enter a number : ";
     cin>>n;
-    for(int i=0; i<n; i++){
-        if(prime(i)==1){
+    for(int i=2; i<n; i++){
+        if(prime(i)){
             count++;
         }
     }
