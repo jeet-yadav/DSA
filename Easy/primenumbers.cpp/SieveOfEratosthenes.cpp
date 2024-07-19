624227b6aa775a20028e858ba71364d5ab6ce64e
jeet-yadav <jeetyadav76468976@gmail.com>
2024-07-19 13:15:21 +0530

    add comments, for better understanding

--- Easy/primenumbers.cpp/SieveOfEratosthenes.cpp
+++ Easy/primenumbers.cpp/SieveOfEratosthenes.cpp
@@ -1,18 +1,19 @@
 #include<iostream>
 #include<vector>
+#include<cstring>
 using namespace std;
 
-int SieveOfEratosthenes(int n){
+int SieveOfEratosthenes(int n){    //using vector
     int count = 0;
     vector<bool> prime(n+1, true); // a bool type vector named prime of size n+1, all the elements are initialised as true for now
     
-    prime[0] = prime[1] = false; // since 0 and 1 are not prime numbers
+    prime[0] = prime[1] = false;   // since 0 and 1 are not prime numbers
     
     for(int i=2; i<n; i++){
-        if(prime[i]){
+        if(prime[i]){              // already we have declared all elements as prime
             count++;
             
-            for(int j=2*i; j<n; j+=i){
+            for(int j=2*i; j<n; j+=i){//    multiples of i are marked as non prime/false
                 prime[j] = 0;
             }
         }
