2437f4d6e3389348cfd7772a7ec732690a8bdffd
jeet-yadav <jeetyadav76468976@gmail.com>
2024-07-20 19:06:44 +0530

    added almost everything about pointers

--- Basic/pointers/pointers.cpp
+++ Basic/pointers/pointers.cpp
@@ -1,8 +1,42 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int num = 5;
-    cout<<num<<endl;
-    cout<<&num<<endl;
+    // int num = 5;
+    // cout<<"num is "<<num<<endl;
+    // cout<<"address of num is "<<&num<<endl;
+
+    // int *p = &num;
+    // cout<<"p is "<<p<<endl;
+    // cout<<"value at address p "<<*p<<endl;
+
+    // cout<<sizeof(num)<<endl;
+    // cout<<sizeof(p)<<endl;
+
+    // cout<<"num is "<<num<<endl;
+    // // (*p)++;
+    // (*p)*=2;
+    // cout<<"num after *p++ is "<<num<<endl;
+
+    // int arr[5] = {5,10,15,20,25};
+    // cout<<"address of arr[0] is "<< arr <<endl;
+    // cout<<"address of arr[0] is "<< &arr[0] <<endl;
+    // cout<<"value at address of arr[0] is "<< *arr <<endl;
+
+    // cout<< *(arr+1) <<endl;
+    // cout<< (*arr)+1 <<endl;
+
+    // int i = 2;
+    // cout<< i[arr] <<endl;//arr[i] == *(arr+i) == i[arr] == *(i+arr)
+    
+    // int *ptr = &arr[0];
+    // cout<<sizeof(arr) <<endl;
+    // cout<<sizeof(ptr) <<endl;
+
+    // cout<< arr <<endl;
+    // cout<< &arr <<endl;
+    // cout<< &arr[0] <<endl;
+    // cout<< arr <<endl;
+
+    
     return 0;
-} 
+}  
