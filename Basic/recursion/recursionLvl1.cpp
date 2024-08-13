2dd0ee5cb77ddc9533c10776027dce66b4f0c823
jeet-yadav <jeetyadav76468976@gmail.com>
2024-08-13 14:08:04 +0530

    tail and head, recursion

--- Basic/recursion/recursionLvl1.cpp
+++ Basic/recursion/recursionLvl1.cpp
@@ -1,15 +1,33 @@
 #include<iostream>
 using namespace std;
 
-int fact(int n){
-    if(n == 1 )
+int fact(int n){//factorial of n
+    if(n == 0)//base case
         return 1;
-    return n * fact(n-1);
+    return n * fact(n-1);//recurrence relation
+}
+
+int power(int n){//2 power n
+    if(n == 0){
+        return 1;
+    }
+    return 2 * power(n-1);
+}
+
+void count(int n){
+    if(n == 0){
+        return;
+    }
+    // cout<<n<<" "; tail recursion
+    count(n-1);
+    cout<<n<<" ";// head recursion
 }
 
 int main(){
     int n;
     cin >> n;
-    cout<< fact(n) <<endl;
+    // cout<< fact(n) <<endl;
+    // cout<< power(n) <<endl;
+    count(n);
     return 0;
 }
