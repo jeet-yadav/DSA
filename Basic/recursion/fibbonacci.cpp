6dea6cbe8c0bf118df6b00c28b4c39e451f0b405
jeet-yadav <jeetyadav76468976@gmail.com>
2024-08-14 22:58:56 +0530

    added fibbonacci file

---
+++ Basic/recursion/fibbonacci.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int fibbo(int n){//has higher complexity
+    if(n == 0){
+        return 0;
+    }
+    if(n == 1){
+        return 1;
+    }
+    int ans = fibbo(n-1) + fibbo(n-2);
+    return ans;  
+}
+
+int main(){
+    int n = 10;
+    int a = fibbo(n);
+    cout<<a<<endl;
+
+    int ans = 0;
+    int x = 0, y = 1;
+    for(int i=0; i<=n; i++){// have lower complexity
+        cout<<ans<<" ";
+        x = y;
+        y = ans;
+        ans = x + y;
+    }
+    return 0;
+}
