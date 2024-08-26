c2febe767fca83d7bc54fdb64c48164f73b2d80e
jeet-yadav <jeetyadav76468976@gmail.com>
2024-08-26 23:24:41 +0530

    added new file a leetcode problem XD

--- Basic/recursion/tempCodeRunnerFile.cpp
+++ Basic/recursion/tempCodeRunnerFile.cpp
@@ -1,8 +1,1 @@
-
-// int nums(int n){
-//     char arr[10] = {"zero","one","two","three","four","five","six","seven","eight","nine"};
-//     int num = n % 10;
-//     cout<<arr[num];
-//     // n = n / 10;
-//     nums(nums/10);
-// }
+arr[s]
---
+++ Easy/subsets.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void subSet(vector<int>& arr, vector<vector<int>>& arr2, int index, vector<int>& output){
+    if(index >= arr.size()){
+        arr2.push_back(output);
+        return;
+    }
+    output.push_back(arr[index]);
+    subSet(arr,arr2,index+1,output);
+
+    output.pop_back();
+    subSet(arr,arr2,index+1,output);
+}
+int main(){
+    vector<int> arr = {1,2,3};
+    vector<vector<int>> arr2;
+    vector<int> output;
+    int index = 0;
+
+    subSet(arr,arr2,index,output);
+
+    for (int i=0; i<arr2.size(); i++){
+        cout<<"-> ";
+        for (int j=0; j<arr2[i].size(); j++){
+            cout << arr2[i][j] << " ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
