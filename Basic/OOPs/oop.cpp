c1c85bf428e9fed08b8fd293edcac1e060ba2bca
jeet-yadav <jeetyadav76468976@gmail.com>
2024-09-03 16:27:55 +0530

    added getter, setter

--- Basic/OOPs/oop.cpp
+++ Basic/OOPs/oop.cpp
@@ -4,14 +4,25 @@ using namespace std;
 
 class Hero {// user defined datatype
     // properties
-    public:
+    private:// access modifiers
     int health;
+
+    public:
     char level;
+    int getHealth(){// getter
+        return health;
+    }
+    void sethealth(int h){// setter
+        health = h;
+    }
 };
 
 int main(){
     Hero h1;
-    cout<<h1.health<<endl;
+    h1.sethealth(70);
+    cout<<h1.getHealth()<<endl;
+    h1.level = 'A';
+    // cout<<h1.health<<endl;
     cout<<h1.level<<endl;
     return 0;
 }
