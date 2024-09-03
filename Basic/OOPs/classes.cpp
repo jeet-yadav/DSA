a4253eb0a16af3534e24e812e460a037094bae9c
jeet-yadav <jeetyadav76468976@gmail.com>
2024-09-03 21:13:10 +0530

    added constructors

--- Basic/OOPs/classes.cpp
+++ Basic/OOPs/classes.cpp
@@ -1,10 +1,17 @@
 #include<iostream>
 using namespace std;
 
-class Superhero{
-
+class Hero {
+    public:
+    char level;
+    int health;
 };
 
 int main(){
+    Hero h1;
+   cout<<sizeof(h1)<<endl;
+   //size shows 8 bit due to padding of 3 bits exta in int health, which takes 4 bytes
+   //This padding is included beacause of memory alignment, which makes program faster
+   //4 bits of storage is stored in multiple of 4, so extra 3 bits are added as padding 
     return 0;
 }
--- Basic/OOPs/oop.cpp
+++ Basic/OOPs/oop.cpp
@@ -15,14 +15,33 @@ class Hero {// user defined datatype
     void sethealth(int h){// setter
         health = h;
     }
+    Hero(){// constructor, called when object is created
+        cout<<"constructor called"<<endl;
+    }
+    Hero(int health){
+        cout<<"this -> "<<this<<endl;// this is a pointer to current object
+        this -> health = health;
+    }
 };
 
 int main(){
     Hero h1;
-    h1.sethealth(70);
-    cout<<h1.getHealth()<<endl;
     h1.level = 'A';
-    // cout<<h1.health<<endl;
+    h1.sethealth(70);
     cout<<h1.level<<endl;
+    cout<<h1.getHealth()<<endl;
+
+    Hero *h2 = new Hero(99);// using heap memory, Static memory allocation
+    h2->level = 'B';
+    // h2->sethealth(80);
+    cout<<(*h2).level<<endl;
+    cout<<(*h2).getHealth()<<endl;
+    // also can be written as
+    // cout<< h2->level <<endl;
+    // cout<< h2->getHealth() <<endl;
+
+    Hero h3(10);// argument constructor
+    cout<<h3.getHealth()<<endl;
+    cout<<"h3 address -> "<<&h3<<endl;
     return 0;
 }
