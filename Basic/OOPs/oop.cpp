93f2444538e54ddaac7b1ea484b9740b384dfc7d
jeet-yadav <jeetyadav76468976@gmail.com>
2024-09-04 18:50:29 +0530

    made it more clear

--- Basic/OOPs/oop.cpp
+++ Basic/OOPs/oop.cpp
@@ -18,7 +18,7 @@ class Hero {// user defined datatype
     Hero(){// constructor, called when object is created
         cout<<"constructor called"<<endl;
     }
-    Hero(int health){
+    Hero(int health){//parameterized constructor, called when object is created with argument
         cout<<"this -> "<<this<<endl;// this is a pointer to current object
         this -> health = health;
     }
@@ -26,22 +26,24 @@ class Hero {// user defined datatype
 
 int main(){
     Hero h1;
+    cout<<"h1 address -> "<<&h1<<endl;
     h1.level = 'A';
     h1.sethealth(70);
     cout<<h1.level<<endl;
-    cout<<h1.getHealth()<<endl;
+    cout<<h1.getHealth()<<endl<<endl;
 
     Hero *h2 = new Hero(99);// using heap memory, Static memory allocation
+    cout<<"h2 address -> "<<h2<<endl;
     h2->level = 'B';
     // h2->sethealth(80);
     cout<<(*h2).level<<endl;
-    cout<<(*h2).getHealth()<<endl;
+    cout<<(*h2).getHealth()<<endl<<endl;
     // also can be written as
     // cout<< h2->level <<endl;
     // cout<< h2->getHealth() <<endl;
 
     Hero h3(10);// argument constructor
-    cout<<h3.getHealth()<<endl;
     cout<<"h3 address -> "<<&h3<<endl;
+    cout<<h3.getHealth()<<endl;
     return 0;
 }
