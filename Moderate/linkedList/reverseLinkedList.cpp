cd7efe6a70437fec87adf4ca79a1821b873ad647
jeet-yadav <jeetyadav76468976@gmail.com>
2024-09-12 19:44:12 +0530

    added reversing in linked list

---
+++ Moderate/linkedList/reverseLinkedList.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+    ~Node(){
+        int value = this -> data;
+        if(this -> next != NULL){
+            delete next;
+            next = NULL;
+        }
+    }
+};
+
+void insert(Node* &head, int d){
+    Node* temp = new Node(d);
+    temp -> next = head;
+    head = temp;
+}
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<<temp -> data<<" ";
+        temp = temp -> next;
+    }
+    cout<<endl;
+}
+
+void reverse(Node* &head){
+    Node* prev = NULL;
+    Node* curr = head;
+    Node* forward = NULL;
+
+    while(curr != NULL){
+        forward = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = forward;
+    }
+    head = prev;
+}
+
+int main(){
+    Node* node1 = new Node(1);
+    Node* head = node1;
+
+    for(int i=2; i<=5; i++){
+        insert(head, i);
+    }
+    print(head);
+    reverse(head);
+    print(head);
+    return 0;
+}
