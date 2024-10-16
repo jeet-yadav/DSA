7c2b68bd64a1ec4e403d300dea1384583a9167ee
jeet-yadav <jeetyadav76468976@gmail.com>
2024-10-16 14:23:49 +0530

    easy question done

--- Moderate/queues/reverseFirstK.cpp
+++ Moderate/queues/reverseFirstK.cpp
@@ -6,13 +6,43 @@ using namespace std;
 //https://www.geeksforgeeks.org/problems/queue-reversal/1
 
 int main(){
-    queue<int> q;
+    deque<int> q;
+    int k = 3; // Number of elements to reverse from the front
 
-    // taking input in queue
+    // taking input in deque (1, 2, 3, 4, 5)
     for(int i=1; i<=5; i++){
-       q.push(i);
+       q.push_back(i);
     }
 
+    // Step : 1 Create a stack which will have elements of q till k
+    stack<int> s;
 
+    // Step : 2 Pop the first k elements from the deque and push them into the stack
+    while(k <= q.size()){
+        s.push(q.front());
+        q.pop_front();
+    }
+    // for(int i=0; i<k; i++){
+    //     s.push(q.front());
+    //     q.pop_front();
+    // }
+    
+    // Step : 3 Push the reversed elements from the stack back into the deque
+    while(!s.empty()){
+        q.push_back(s.top());
+        s.pop();
+    }
+
+    // Step : 4 To restore the order of the remaining elements, rotate the last (n-k) elements
+    for(int i=0; i<q.size()-k; i++){
+        q.push_back(q.front());
+        q.pop_front();
+    }
+
+    while(!q.empty()){
+        cout<<q.front()<<" ";
+        q.pop_front();
+    }
+    
     return 0;
 }
